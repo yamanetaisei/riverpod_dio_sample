6cdb800b406701774c31bc183161a4a47a1690dd
taisei <swiftmantaisei@gmail.com>
2022-08-16 01:30:50 +0900

    NewsAPIClientを実装
    まだクエリを設定していない

---
+++ lib/network/news_api_client.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:riverpod_dio_sample/model/news_api_response.dart';
+
+class NewsAPIClient {
+  Future<NewAPIResponse?> fetchNewAPIResponse() async {
+    final dio = Dio();
+    const url = "https://newsapi.orgv2/everything";
+    final response = await dio.get(url);
+
+    if (response.statusCode == 200) {
+      try {
+        final dataWith = response.data as dynamic;
+        final data = dataWith.map((e) => NewAPIResponse.fromJson(e));
+        return data;
+      } catch(e) {
+        rethrow;
+      }
+    }
+    return null;
+  }
+}
